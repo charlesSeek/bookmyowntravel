3e0b18b21cb038de75c614cd8fd34d436d60bea9
shucheng cui <cuishucheng@gmail.com>
2016-11-13 10:04:40 +1100

    remove config.js

--- server/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'secret':'bookmyowntravel',
-    'accessKeyId': 'AKIAJRZW2IIRT4CIKMNQ',
-    'secretAccessKey': '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',
-    'region':'us-west-2'
-    
-}
